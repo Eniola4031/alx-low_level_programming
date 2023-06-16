942a68f8df29e9159e75db1637e137e016a6b1a8
eniola4031 <agboola.eniola4031@gmail.com>
2023-06-16 04:40:02 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - main entry point
+* return; always zero or success
+*/
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of interger: %lu byte(s)\n", sizeof(int));
+	printf("Size of long interger: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long integer: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of float:%lu byte(s)", sizeof(float));
+	return (0);
+}	
