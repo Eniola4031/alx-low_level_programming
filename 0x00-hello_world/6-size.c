d6f1b2c15147d71ddfb518b921b5d083fe9c8705
eniola4031 <agboola.eniola4031@gmail.com>
2023-06-16 05:26:29 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
- * main - main entry point
- * Return: always 0 (success)
- */
-
+*main - main entry point
+*Return: always 0(success)
+*/
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of interger: %lu byte(s)\n", sizeof(int));
-	printf("Size of long interger: %lu byte(s)\n", sizeof(long int));
-	printf("Size of long long integer: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of float:%lu byte(s)\n", sizeof(float));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }	
