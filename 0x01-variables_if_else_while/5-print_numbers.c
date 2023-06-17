ee57a7502e6c3e67cb8180f4e2cf8e10cbbc6e11
eniola4031 <agboola.eniola4031@gmail.com>
2023-06-17 15:59:49 -0700

    base 10 numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,5 +4,15 @@
 * Description: ‘print  alphabets’
 * Return: always 0 (success)
 */
+int main(void)
+{
+	int n;
 
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
 
