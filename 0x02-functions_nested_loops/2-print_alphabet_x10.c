816732f6a5bfb7762f67ff1cbe9c149d6f5c8621
eniola4031 <agboola.eniola4031@gmail.com>
2023-06-21 18:23:16 -0700

    fixes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_alphabet_x10 - print alphabet 10 times
  *
@@ -7,9 +6,8 @@
 */
 void print_alphabet_x10(void)
 {
-	int i;
-	int j;
-	
+int i;
+int j;
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 97; j <= 122; j++)
