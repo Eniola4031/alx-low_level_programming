1d54f5fde1c6cf66069e87512e5a52be3357ab52
eniola4031 <agboola.eniola4031@gmail.com>
2023-10-20 13:51:32 -0700

    removed space

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,16 +4,15 @@
 /**
  * free_list - frees a linked list
  * @head: list_t list to be freed
- */
+*/
 	void free_list(list_t *head)
 {
- 		list_t *temp;
-
+	list_t *temp;
 	while (head)
- 	{
- 		temp = head->next;
- 		free(head->str);
- 		free(head);
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
 		head = temp;
 	}
 }
